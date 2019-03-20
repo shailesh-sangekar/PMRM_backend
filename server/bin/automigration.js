ab2540ac9796f9714b9c72a4d9207169189b1544
shailesh-sangekar <shailesh.sangekar@theblueflamelabs.com>
2019-03-20 16:15:13 +0530

    First commit

---
+++ server/bin/automigration.js
@@ -0,0 +1,30 @@
+/* eslint-disable strict */
+var app = require('../server');
+
+// this loads the accountDb configuration in ~/server/datasources.json
+var dataSource = app.dataSources.postgres;
+
+// this automigrates the model
+dataSource.automigrate('Accountmanagement', function(err) {
+  if (err) throw err;
+  dataSource.disconnect();
+});
+dataSource.automigrate('Deapartmentmasters', function(err) {
+  if (err) throw err;
+  dataSource.disconnect();
+});
+
+dataSource.automigrate('Country', function(err) {
+  if (err) throw err;
+  dataSource.disconnect();
+});
+/*
+dataSource.isActual(models, function(err, actual) {
+  if (!actual) {
+    dataSource.autoupdate(models, function(err, result) {
+      if (err) throw err;
+      dataSource.disconnect();
+    });
+  }
+});
+*/
